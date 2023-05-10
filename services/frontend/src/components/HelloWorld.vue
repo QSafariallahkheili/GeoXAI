40d5a3af7c82469688fee85486ba73a6eea20c7b
QSafariallahkheili <safari.qassem@gmail.com>
2023-05-10 14:14:10 +0200

    frontend init

---
+++ services/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div ref="mapContainer" style="height: 100vh;">
+  </div>
+</template>
+
+<script setup>
+import { Map } from 'maplibre-gl';
+import axios from 'axios';
+import { ref, onMounted, onUnmounted } from "vue";
+
+const mapContainer = ref(null);
+const map = ref(null);
+
+onMounted(() => {
+
+  map.value = new Map({
+    container: mapContainer.value,
+    style: 'https://api.maptiler.com/maps/a2eb63ba-7d0e-4b25-9cfc-9ef74d786ec4/style.json?key=XgdreUwN4V3uEHHZHsWO',
+    center: [ 13.259101,52.538625],
+    zoom: 7,
+  });
+
+  axios.get('/')
+  .then((res) => {
+    console.log(res.data)
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+  
+    
+})
+
+onUnmounted(() => {
+      if (map.value) {
+        map.value.remove();
+      }
+});
+
+</script>
+
+<style scoped>
+
+</style>
