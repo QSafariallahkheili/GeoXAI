31f1e442a60dfc221716f8f0d19cf89ee5130bb9
QSafariallahkheili <safari.qassem@gmail.com>
2023-05-11 17:17:01 +0200

    adjust db host to docker container name in production

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 dbConfig = {
-    'host': getenv('DB_HOST', 'localhost'),
+    'host': getenv('DB_HOST', 'brandenburg_db'),
     'port': getenv('DB_PORT', 5432),
     'dbname': getenv('DB_NAME', 'brandenburg'),
     'user': getenv('DB_USER', 'berlinberlin'),
