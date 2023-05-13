8f5cef90e7875e7ce08786e9e75e2b7566b68a65
QSafariallahkheili <safari.qassem@gmail.com>
2023-05-13 14:01:42 +0200

    allowed origins

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://193.175.29.219:8080"],
+    allow_origins=["http://localhost:8080","http://localhost", "http://193.175.29.219:8080", "http://127.0.0.1", "http://193.175.29.219"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
