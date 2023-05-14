61ed4e512f5ccba44a28e2fb12f47f1ae6494d4d
QSafariallahkheili <safari.qassem@gmail.com>
2023-05-14 19:04:30 +0200

    test runnig nginx after push

--- services/frontend/src/components/HelloWorld.vue
+++ services/frontend/src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ const mapContainer = ref(null);
 const map = ref(null);
 
 onMounted(() => {
-  console.log(process.env.VUE_APP_BASE_URI)
+  console.log(process.env.VUE_APP_BASE_URI , "test push")
   map.value = new Map({
     container: mapContainer.value,
     style: 'https://api.maptiler.com/maps/a2eb63ba-7d0e-4b25-9cfc-9ef74d786ec4/style.json?key=XgdreUwN4V3uEHHZHsWO',
