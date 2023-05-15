a1b23779d9f99f85d2f98aa8a945b86d994d31a4
QSafariallahkheili <safari.qassem@gmail.com>
2023-05-15 08:16:07 +0200

    app title

--- services/frontend/vue.config.js
+++ services/frontend/vue.config.js
@@ -12,7 +12,7 @@ module.exports = defineConfig({
       filename: 'index.html',
       // when using title option,
       // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'LigFinder',
+      title: 'Innowest',
       // chunks to include on this page, by default includes
       // extracted common chunks and vendor chunks.
       chunks: ['chunk-vendors', 'chunk-common', 'index']
