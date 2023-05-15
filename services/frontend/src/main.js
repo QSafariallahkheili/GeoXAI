737ce7ae9336295120130819c4d91d86bd91dcdd
QSafariallahkheili <safari.qassem@gmail.com>
2023-05-15 15:39:22 +0200

    import maplibre.css in main.js file

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { createApp } from 'vue';
 import axios from 'axios';
 import { createPinia } from "pinia";
-
+import '../node_modules/maplibre-gl/dist/maplibre-gl.css';
 import App from './App.vue';
 import router from './router';
 
