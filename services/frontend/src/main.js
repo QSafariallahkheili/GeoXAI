876bee6bef633fc7f7d7a14d026bff699389510b
QSafariallahkheili <safari.qassem@gmail.com>
2023-05-15 18:14:13 +0200

    add vuetify support to the existing project

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -6,10 +6,23 @@ import '../node_modules/maplibre-gl/dist/maplibre-gl.css';
 import App from './App.vue';
 import router from './router';
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
 const app = createApp(App);
 
 axios.defaults.withCredentials = true;
 
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 app.use(router);
 app.use(createPinia());
+app.use(vuetify);
 app.mount("#app");
