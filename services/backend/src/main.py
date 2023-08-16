8746c869d4cc5f114ca099b7a8cb5e7074e27c0f
QSafariallahkheili <safari.qassem@gmail.com>
2023-08-16 14:37:32 +0200

    retrieve layer and their geom type

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -34,7 +34,25 @@ def connect():
 def home():
     conn = connect()
     cur = conn.cursor()
-    cur.execute("""select table_name from information_schema.columns where column_name = 'geom' """)
+    cur.execute(""" 
+        with table_specification as (SELECT 
+            t.table_name,
+            regexp_replace(
+                regexp_replace(format_type(a.atttypid, a.atttypmod),'[0-9\(\)]+','', 'g'),
+                'geometry|,',
+                '',
+                'g'
+            ) AS data_type
+        FROM 
+            information_schema.tables t
+        JOIN 
+            pg_attribute a ON t.table_name = a.attrelid::regclass::text
+        WHERE 
+        t.table_schema = 'public'
+        AND format_type(a.atttypid, a.atttypmod) LIKE 'geometry%')
+        
+        select * from table_specification where length(data_type)>0; """
+    )
     user = cur.fetchall()
     cur.close()
     conn.close()
