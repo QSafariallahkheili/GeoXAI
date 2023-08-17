830a674b1f9609f03a62cab1e55cf08916af839e
QSafariallahkheili <safari.qassem@gmail.com>
2023-08-17 10:43:10 +0200

    popup functionality on click on features

--- services/frontend/src/components/HelloWorld.vue
+++ services/frontend/src/components/HelloWorld.vue
@@ -6,20 +6,22 @@
 </template>
 
 <script setup>
-import { Map } from 'maplibre-gl';
+import { Map, Popup } from 'maplibre-gl';
 import { ref, onMounted, onUnmounted } from "vue";
 import { storeToRefs } from 'pinia'
 import { useMapStore } from '../stores/map'
 import LayerUI from "@/components/LayerUI.vue";
+import { createHTMLAttributeTable } from '../utils/createHTMLAttributeTable';
+
 const { center, zoom, style } = storeToRefs(useMapStore())
 
 const mapContainer = ref(null);
-const map = ref(null);
+let map = null;
 
 
 onMounted(() => {
 
-  map.value = new Map({
+  map = new Map({
     container: mapContainer.value,
     style: style.value,
     center: center.value,
@@ -32,7 +34,8 @@ const addLayerToMap = (clickedLayerName, layerType, style)=>{
   let vectorSource = "public"+"."+clickedLayerName;
   let vectorSourceLayer = "public"+"."+clickedLayerName;
   let vectorUrl = vectorServer + vectorSourceLayer + "/{z}/{x}/{y}.pbf";
-  map.value.addSource(vectorSource, {
+  
+    map.addSource(vectorSource, {
       "type": "vector",
       "tiles": [vectorUrl],
       "minzoom": 0,
@@ -45,29 +48,56 @@ const addLayerToMap = (clickedLayerName, layerType, style)=>{
       "type": layerType.value,
       "paint":  style.value
   };
-  map.value.addLayer(layer)  
+  map.addLayer(layer)  
+  let popup = new Popup()
+  map.on('click', "public"+"."+clickedLayerName, function(e) {
+    
+    const coordinates = [e.lngLat.lng, e.lngLat.lat]
+    console.log(e.features[0].properties)
+    popup.setLngLat(coordinates)
+    popup.setDOMContent(
+      createHTMLAttributeTable(
+        e.lngLat.lng,
+        e.lngLat.lat,
+        e.features[0].properties
+      )
+    )
+    popup.addTo(map);
+
+    // modifyinh popup's default style
+    document.getElementsByClassName('maplibregl-popup-content')[0].style.borderRadius="8px"
+    document.getElementsByClassName('maplibregl-popup-content')[0].style.width="fit-content"
+
+  });
+  map.on('mouseenter', "public"+"."+clickedLayerName, function() {
+    map.getCanvas().style.cursor = 'pointer';
+  });
+  map.on('mouseleave', "public"+"."+clickedLayerName, function() {
+    map.getCanvas().style.cursor = '';
+  });
+
  
 }
 const toggleLayerVisibility = (clickedLayerName)=>{
-    let visibility = map.value.getLayoutProperty(
+    let visibility = map.getLayoutProperty(
     'public'+'.'+clickedLayerName,
     'visibility'
   );
   if (visibility == 'visible'){
-    map.value.setLayoutProperty('public'+'.'+clickedLayerName,'visibility', 'none')
+    map.setLayoutProperty('public'+'.'+clickedLayerName,'visibility', 'none')
   }
   else if (visibility == undefined){
-    map.value.setLayoutProperty('public'+'.'+clickedLayerName,'visibility', 'none')
+    map.setLayoutProperty('public'+'.'+clickedLayerName,'visibility', 'none')
   }
   else {
-    map.value.setLayoutProperty('public'+'.'+clickedLayerName,'visibility', 'visible')
+    map.setLayoutProperty('public'+'.'+clickedLayerName,'visibility', 'visible')
   }
 
 }
 
 onUnmounted(() => {
-      if (map.value) {
-        map.value.remove();
+      if (map) {
+        map.remove();
       }
 });
 
