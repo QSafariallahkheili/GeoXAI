eaff935ea3e35750cf2bcdeecb36a69b7698ff44
QSafariallahkheili <safari.qassem@gmail.com>
2023-08-23 15:58:03 +0200

    background for app log

--- services/frontend/src/components/AppLogo.vue
+++ services/frontend/src/components/AppLogo.vue
@@ -1,10 +1,8 @@
 <template>
-    <div class="logo-ui">
-      
-        <v-img class="ml-1" src="innowest-logo.png" width="80px"> 
+    <v-card class="logo-ui">
+        <v-img class="logo-img" src="innowest-logo.png" width="80px"> 
         </v-img>
-
-    </div>
+    </v-card>
 </template>
 <script setup>
 
@@ -18,6 +16,15 @@
     right: 10px;
     top: 10px;
     z-index:10;
+    background: white;
+    background-color: rgba(255,255,255,0.6);
+    backdrop-filter: blur(5px);
+    -webkit-backdrop-filter: blur(5px);
+    -moz-backdrop-filter: blur(5px);
+    -ms-backdrop-filter: blur(5px);
+}
+.logo-img{
+    margin:10px
 }
 
 </style>
