dae0816c0c68814178a5ee0e2b0558ea069eb045
QSafariallahkheili <safari.qassem@gmail.com>
2023-08-23 15:11:35 +0200

    create Dialog component

---
+++ services/frontend/src/components/MetadataDialog.vue
@@ -0,0 +1,74 @@
+<template>
+    <v-row justify="center">
+        <v-dialog
+            v-model="dialog"
+            max-width="500"
+            style="border-radius:20px"
+        >
+        
+            <v-card class="dialog-ui">
+                <v-card-title>
+                    <span class="text-h6">{{ tablename }} meta data</span>
+                </v-card-title>
+                <v-divider></v-divider>
+
+                <div v-if="metadata">
+                    <div v-for="(item,index) in metadata" :key="index">
+                        <v-card-text v-if="item">
+                            <span  v-if="isValidURL(item)">
+                                {{index}}: <a :href="metadata.source" target="_blank">{{ item }}</a> 
+                            </span>
+                            <span v-else>
+                                {{index}}: {{ item }}
+                            </span>
+                        
+                        </v-card-text>
+                    
+                    </div>
+                </div>
+                <template v-else>
+                    <v-card-text>
+                        No metaData provided for {{ tablename }}
+                    </v-card-text>
+                </template>
+            
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                    color="blue-darken-1"
+                    variant="text"
+                    @click="dialog = false"
+                    >
+                    Close
+                    </v-btn>
+                    
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-row>
+  </template>
+
+<script setup>
+
+import { storeToRefs } from 'pinia'
+import { useMetadataDialogStore } from '../stores/metadataDialog'
+import { isValidURL } from '../utils/isValidURL';
+
+
+const { dialog, metadata, tablename } = storeToRefs(useMetadataDialogStore())
+
+</script>
+
+<style scoped>
+.v-dialog > .v-overlay__content > .v-card {
+    display: flex;
+    flex-direction: column;
+    border-radius: 20px;
+    background-color: rgba(255,255,255,0.6);
+    backdrop-filter: blur(5px);
+    -webkit-backdrop-filter: blur(5px);
+    -moz-backdrop-filter: blur(5px);
+    -ms-backdrop-filter: blur(5px);
+    border-radius: 8px;
+}
+</style>
