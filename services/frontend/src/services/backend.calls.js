0d80eb1777c2c96a9395030024736470c736d9f1
QSafariallahkheili <safari.qassem@gmail.com>
2023-09-22 13:26:46 +0200

    API calls

--- services/frontend/src/services/backend.calls.js
+++ services/frontend/src/services/backend.calls.js
@@ -4,3 +4,13 @@ export async function getTableNames() {
     const response = await HTTP.get("")
     return response.data;
   }
+
+export async function getIndicatorNames() {
+  const response = await HTTP.get("/indcators_list")
+  return response.data;
+}
+
+export async function getIndicatorData(indicator) {
+  const response = await HTTP.post("get_indicatort_data", indicator);
+  return response.data;
+}
