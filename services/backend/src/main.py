ef465e8067faba4e343d12762c2c9427391fa23a
qsafariallahkheili <safari.qassem@gmail.com>
2023-11-23 14:19:30 +0100

    default db username

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -19,7 +19,7 @@ dbConfig = {
     'host': getenv('DB_HOST', 'localhost'),
     'port': getenv('DB_PORT', 5432),
     'dbname': getenv('DB_NAME', 'brandenburg'),
-    'user': getenv('DB_USER', 'berlinberlin'),
+    'user': getenv('DB_USER', 'postgres'),
     'password': getenv('DB_PASSWORD', '1234')
 }
 
