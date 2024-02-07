95e227f9a7be61dae7d54f7cd42f7d6e30d93675
qsafariallahkheili <safari.qassem@gmail.com>
2024-02-07 15:07:49 +0100

    alter access origin and prod env

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080","http://localhost", "http://193.175.29.219:8080", "http://127.0.0.1", "http://193.175.29.219", "http://tv4-geo.innowest-brandenburg.de:8080", "http://tv4-geo.innowest-brandenburg.de"],
+    allow_origins=["http://localhost:8080","http://localhost", "http://194.94.235.195:8080", "http://127.0.0.1", "http://194.94.235.195"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
