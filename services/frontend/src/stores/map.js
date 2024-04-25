eb0e3d8ca0071b1dbd3cb4ced9aa14e86ae51e09
qsafariallahkheili <safari.qassem@gmail.com>
2024-04-25 08:22:41 +0200

    setting pitch

--- services/frontend/src/stores/map.js
+++ services/frontend/src/stores/map.js
@@ -5,6 +5,7 @@ export const useMapStore = defineStore ({
     state: () => ({
         center: [13.259101,52.538625],
         zoom: 7,
-        style: 'https://api.maptiler.com/maps/a2eb63ba-7d0e-4b25-9cfc-9ef74d786ec4/style.json?key=XgdreUwN4V3uEHHZHsWO'
+        style: 'https://api.maptiler.com/maps/a2eb63ba-7d0e-4b25-9cfc-9ef74d786ec4/style.json?key=XgdreUwN4V3uEHHZHsWO',
+        maxPitch: 85
     })
 })
