525882238405df35dcf9da4f49ff221e222025cf
qsafariallahkheili <safari.qassem@gmail.com>
2025-07-11 09:45:10 +0200

    dark style basemap

--- services/frontend/src/stores/map.js
+++ services/frontend/src/stores/map.js
@@ -5,7 +5,8 @@ export const useMapStore = defineStore ({
     state: () => ({
         center: [12.855993318797012, 52.67491338728945],
         zoom: 7,
-        style: "https://api.maptiler.com/maps/a2eb63ba-7d0e-4b25-9cfc-9ef74d786ec4/style.json?key=XgdreUwN4V3uEHHZHsWO",
+        //style: "https://api.maptiler.com/maps/a2eb63ba-7d0e-4b25-9cfc-9ef74d786ec4/style.json?key=XgdreUwN4V3uEHHZHsWO",
+        style: "https://api.maptiler.com/maps/58fde0bf-e9f9-431e-8992-db2503cd5a1b/style.json?key=XgdreUwN4V3uEHHZHsWO",
         maxPitch: 85
     })
 })
