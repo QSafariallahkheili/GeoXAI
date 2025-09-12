0b4cb7ebc5ebf887fbb213c810030acd6e4fa42b
qsafariallahkheili <safari.qassem@gmail.com>
2025-09-12 14:37:52 +0200

    raster legend label margin

--- services/frontend/src/stores/mapLegend.js
+++ services/frontend/src/stores/mapLegend.js
@@ -31,7 +31,7 @@ export const useMapLegendStore = defineStore ({
         },
         setActivatedLegend(visStatus,layerId){
             if (visStatus=='visible'){
-                this.rasterLegendUrl=process.env.VUE_APP_GEOSERVER_URL+'/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=15&HEIGHT=15&LAYER=geoxai:'+layerId+'&legend_options=fontSize:12;countMatched:true;fontAntiAliasing:true&Transparent=True'
+                this.rasterLegendUrl=process.env.VUE_APP_GEOSERVER_URL+'/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=15&HEIGHT=15&LAYER=geoxai:'+layerId+'&legend_options=dx:10;dy:0;fontSize:12;countMatched:true;fontAntiAliasing:true&Transparent=True'
                 this.rasterLegendTitle = layerId
             }
             else {
