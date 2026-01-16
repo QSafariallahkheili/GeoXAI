ac22e6aaa36aecbb9b63e45694cec006ac929c7b
qsafariallahkheili <safari.qassem@gmail.com>
2026-01-16 12:59:12 +0100

    add questionnaire path

--- services/frontend/src/router/index.js
+++ services/frontend/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import QuestionnaireUI from '../views/QuestionnaireUI.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/questionnaire',
+    name: 'Questionnaire',
+    component: QuestionnaireUI
   }
 ]
 
---
+++ services/frontend/src/views/QuestionnaireUI.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="questionnaire">
+    <h1>Questionnaire</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
