1b78204c84fc488b572977a1f734d5196f90562a
qsafariallahkheili <safari.qassem@gmail.com>
2026-01-18 09:22:21 +0100

    conditional rendering of components

--- services/frontend/src/views/QuestionnaireUI.vue
+++ services/frontend/src/views/QuestionnaireUI.vue
@@ -1,8 +1,46 @@
 <template>
-  <div class="questionnaire">
-    <h1>Questionnaire</h1>
+  <div class="questionnaire" v-if="currentStep !== null">
+      <WelcomeNote 
+        v-if="currentStep === 0" 
+      />
+
+      <TutorialNote 
+        v-if="currentStep === 1" 
+      />
+
+      <BackgroundUI 
+        v-if="currentStep === 2" 
+      />
+    
   </div>
 </template>
 
 <script setup>
+  import { storeToRefs } from 'pinia'
+
+  import WelcomeNote from '@/components/questionnaire/WelcomeNote.vue'
+  import TutorialNote from '@/components/questionnaire/TutorialNote.vue'
+  import BackgroundUI from '@/components/questionnaire/BackgroundUI.vue'
+  import { useQuestionnaireStore } from '../stores/questionnaire'
+  let {currentStep} = storeToRefs(useQuestionnaireStore());
+
+
 </script>
+<style scoped>
+
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: all 0.35s ease;
+}
+
+.slide-fade-enter-from {
+  opacity: 0;
+  transform: translateX(24px);
+}
+
+.slide-fade-leave-to {
+  opacity: 0;
+  transform: translateX(-24px);
+}
+
+</style>
