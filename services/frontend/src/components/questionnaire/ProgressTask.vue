5459f71db75db85927859d675d3aad2031baffcc
qsafariallahkheili <safari.qassem@gmail.com>
2026-01-20 10:50:47 +0100

    task progress UI

---
+++ services/frontend/src/components/questionnaire/ProgressTask.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="progress-container">
+
+      <h2 v-if="progress >= 1">Progress: {{(progress - 1) / 10 * 100}}%</h2>
+    </div>
+</template>
+<script setup>
+import { storeToRefs } from 'pinia'
+import { useQuestionnaireStore } from '../../stores/questionnaire'
+
+let {progress} = storeToRefs(useQuestionnaireStore());
+
+</script>
+<style scoped>
+.progress-container {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+}
+
+
+</style>
