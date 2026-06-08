7461a650738adf27ff410a4d92028303bf9582da
qsafariallahkheili <safari.qassem@gmail.com>
2026-06-08 07:50:42 +0200

    change map center and zoom: to berlin

--- services/frontend/src/stores/map.js
+++ services/frontend/src/stores/map.js
@@ -3,8 +3,8 @@ import { defineStore } from 'pinia'
 export const useMapStore = defineStore ({
     id: 'map',
     state: () => ({
-        center: [12.855993318797012, 52.67491338728945],
-        zoom: 7,
+        center: [13.405910, 52.518824],
+        zoom: 9,
         //style: "https://api.maptiler.com/maps/a2eb63ba-7d0e-4b25-9cfc-9ef74d786ec4/style.json?key=XgdreUwN4V3uEHHZHsWO",
         style: "https://api.maptiler.com/maps/58fde0bf-e9f9-431e-8992-db2503cd5a1b/style.json?key=XgdreUwN4V3uEHHZHsWO",
         maxPitch: 85
